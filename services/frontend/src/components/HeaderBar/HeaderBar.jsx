d0f6ce809f03fa485b203bdcfaddeb9d0aa0a7b6
Lou Brtt <briottetlou@gmail.com>
2026-01-17 19:19:21 +0100

    build(feat/homepage) : refactorizing header bar component by creating searchbar component in itself

--- services/frontend/src/components/HeaderBar/HeaderBar.jsx
+++ services/frontend/src/components/HeaderBar/HeaderBar.jsx
@@ -4,52 +4,10 @@ import './HeaderBar.css'
 import { useNavigate } from 'react-router-dom'
 import { searchPages } from './SearchConfig';
 import Loading from '../Loading/Loading';
+import SearchBar from './SearchBar';
 
 export default function HeaderBar() {
   const navigate = useNavigate();
-  const [searchInput, setSearchInput] = useState("");
-  const [results, setResults] = useState([]);
-  const [selectedIndex, setSelectedIndex] = useState(0);
-  const [isLoading, setIsLoading] = useState(false);
-  const [showResult, setShowResult] = useState(false);
-
-  useEffect(() => {
-    if (searchInput.trim() === ''){
-      setResults([]);
-      setShowResult(false);
-      setIsLoading(false);
-      return ;
-    }
-    try {
-      const searchResult = searchPages(searchInput);
-      setResults(searchResult || []);
-      setShowResult(true);
-      setSelectedIndex(0);
-    } catch (error) {
-      console.error('Search error:', error);
-      setResults([]);
-    }
-  }, [searchInput]);
-
-  const handleSelection = (page) => {
-    if (page.path){
-      setSearchInput('');
-      setShowResult(false);
-      navigate(page.path);
-    }
-  }
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (results.length > 0)
-        handleSelection(results[selectedIndex]);
-  }
-  const handleOnClick = (path) => {
-    setResults([]);
-    setShowResult(false);
-    setSearchInput("");
-    navigate(path);
-  }
-
 
   return(
     <>
@@ -68,36 +26,7 @@ export default function HeaderBar() {
             />
           </form>
         </div>
-        <div className="search-bar">
-          <form onSubmit={handleSubmit}>
-            <label> </label>{' '}
-            <input
-              type="text"
-              className="feild px-4 py-2 rounded-lg"
-              placeholder="Rechercher"
-              onChange={(e) => setSearchInput(e.target.value)}
-              onFocus={() => searchInput && setShowResult(true)}
-            />
-            {showResult && results.length > 0 &&
-            <div className='results'>
-                {results.map((page, index)=> (
-                  <div className='result-search' 
-                  key={page.id}
-                  onMouseEnter={() => setSelectedIndex(index)}
-                  onClick={() => handleSelection(page)}
-                  >
-                    <span className='icon-notification'>{page.icon}</span>
-                    <div className='notif-text'>
-                    <div className='notif-title'><strong>{page.title}</strong></div>
-                    <div>{page.description}</div>
-                    </div>
-                    <span className="text-slate-400 text-sm font-medium uppercase tracking-wider"/>
-                  </div>
-                ))}
-            </div>
-            }
-          </form>
-        </div>
+        <SearchBar/>
         <button className="navbar-btn" onClick={() => handleOnClick("/dashboard/profile")}>
           <img src={profile} />
         </button>
